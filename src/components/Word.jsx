97c95b097aa3be6f2adc4dcea9eac5bcc96b8b5a
aaadriana <adriana@stanciu.uk>
2021-10-28 12:01:32 +0100

    added the functionality for the correct and wrong letters
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Word = () => {
-  return <div className="word"></div>;
+const Word = ({ randomWord, correctLetters }) => {
+  return (
+    <div className="word">
+      {randomWord.split("").map((letter, index) => {
+        return (
+          <span className="letter" key={index}>
+            {correctLetters.includes(letter) ? letter : ""}
+          </span>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Word;
