edfe8eb65a14da19ae85daa3e52204c85a78c77c
aaadriana <adriana@stanciu.uk>
2021-10-28 17:41:01 +0100

    fixed the input focus with blur
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+function handleKeyUp(event) {
+  //key code for enter
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    event.target.blur();
+  }
+}
+
 const SearchUser = ({ setUserName, setStartGame, playAgain }) => {
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
+        event.target.blur();
         setStartGame(true);
         setUserName(event.target.username.value);
         playAgain();
@@ -18,6 +27,7 @@ const SearchUser = ({ setUserName, setStartGame, playAgain }) => {
           placeholder="Github username"
           name="username"
           id="username"
+          onKeyUp={handleKeyUp}
         ></input>
       </label>
       <button type="submit">Play</button>
